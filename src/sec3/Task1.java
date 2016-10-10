aad467230c0fa20be36932db6bc3e87155a3d757
Masaaki Uno <unokun@gmail.com>
2016-10-10 10:13:29 +0900

    java project

--- src/sec3/Task1.java
+++ src/sec3/Task1.java
@@ -2,13 +2,30 @@ package sec3;
 
 public class Task1 {
 
+	/**
+	 * 偶数判定
+	 * @param value 判定する値
+	 * @return 偶数の場合にはtrue、奇数の場合にはfalse
+	 */
+	public static boolean isEven(int value) {
+		return ((value % 2) == 0);
+	}
+	/**
+	 * サイコロをふる
+	 * サイコロの目の計算方法
+	 * Math.randomを使う。
+	 * Math.randomは0以上1未満を返すため6倍して1を加える。
+	 * @return サイコロの目(1以上6以下の整数値)
+	 */
+	public static int throwDice() {
+		return (int) (Math.random() * 6 + 1);
+		
+	}
 	public static void main(String[] args) {
-		// サイコロの目の計算方法
-		// Math.randomは0以上1未満
-		int dice = (int) (Math.random() * 6 + 1);
+		int dice = throwDice();
 		
 		System.out.println("サイコロの数字: " + dice);
-		System.out.println("サイコロの目は" + (((dice % 2) == 0) ? "偶数" : "奇数") + "です");
+		System.out.println("サイコロの目は" + (Task1.isEven(dice) ? "偶数" : "奇数") + "です");
 	}
 
 }
